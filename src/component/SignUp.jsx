d6ebf44f9e71389cc485ac96344c402c5ac38371
yoru.lee <yoru.lee7@gmail.com>
2023-08-15 23:14:30 +0900

    feature : Assignment1

--- src/component/SignUp.jsx
+++ src/component/SignUp.jsx
@@ -1,10 +1,31 @@
 import { React, useState } from "react";
-import { instance } from "../api";
 import axios from "axios";
+import { redirect } from "react-router-dom";
 
 export const SignUp = () => {
-  let [email, setEmail] = useState();
-  let [password, setPassword] = useState();
+  let [email, setEmail] = useState("");
+  let [password, setPassword] = useState("");
+  let [buttonDisabled, setButtonDisabled] = useState(true);
+  let [loggedIn, setLoggedIn] = useState(false);
+
+  const handleEmailChange = (event) => {
+    const newEmail = event.target.value;
+    setEmail(newEmail);
+    updateButtonDisabled(newEmail, password);
+  };
+
+  const handlePasswordChange = (event) => {
+    const newPassword = event.target.value;
+    setPassword(newPassword);
+    updateButtonDisabled(email, newPassword);
+  };
+  //Assignment 1
+  const updateButtonDisabled = (newEmail, newPassword) => {
+    const isValidEmail = newEmail.includes("@");
+    const isValidPassword = newPassword.length >= 8;
+    setButtonDisabled(!isValidEmail || !isValidPassword);
+  };
+
   const createId = async () => {
     const api = await axios.post(
       "https://www.pre-onboarding-selection-task.shop/auth/signup",
@@ -18,7 +39,7 @@ export const SignUp = () => {
         },
       }
     );
-    console.log("api", api);
+    return api.status === 201 ? setLoggedIn(true) : setLoggedIn(false);
   };
 
   return (
@@ -28,16 +49,20 @@ export const SignUp = () => {
         type="email"
         data-testid="email-input"
         value={email}
-        onChange={(e) => setEmail(e.target.value)}
+        onChange={handleEmailChange}
       ></input>
       <label>Password : </label>
       <input
         type="password"
         data-testid="password-input"
         value={password}
-        onChange={(e) => setPassword(e.target.value)}
+        onChange={handlePasswordChange}
       ></input>
-      <button data-testid="signup-button" onClick={createId}>
+      <button
+        data-testid="signup-button"
+        disabled={buttonDisabled}
+        onClick={() => createId()}
+      >
         회원가입
       </button>
     </div>
