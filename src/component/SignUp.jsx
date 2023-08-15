95f0e1cd16da832aeeb8ffc706da050b80135592
yoru.lee <yoru.lee7@gmail.com>
2023-08-16 00:21:26 +0900

    feature : Assignment 2

--- src/component/SignUp.jsx
+++ src/component/SignUp.jsx
@@ -1,12 +1,13 @@
 import { React, useState } from "react";
 import axios from "axios";
-import { redirect } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 export const SignUp = () => {
   let [email, setEmail] = useState("");
   let [password, setPassword] = useState("");
   let [buttonDisabled, setButtonDisabled] = useState(true);
-  let [loggedIn, setLoggedIn] = useState(false);
+
+  const navigate = useNavigate();
 
   const handleEmailChange = (event) => {
     const newEmail = event.target.value;
@@ -27,19 +28,24 @@ export const SignUp = () => {
   };
 
   const createId = async () => {
-    const api = await axios.post(
-      "https://www.pre-onboarding-selection-task.shop/auth/signup",
-      {
-        email: email,
-        password: password,
-      },
-      {
-        headers: {
-          "Content-Type": "application/json",
+    try {
+      const api = await axios.post(
+        "https://www.pre-onboarding-selection-task.shop/auth/signup",
+        {
+          email: email,
+          password: password,
         },
-      }
-    );
-    return api.status === 201 ? setLoggedIn(true) : setLoggedIn(false);
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      // Assignment 2
+      api.status === 201 ? navigate("/signin") : navigate("/signup");
+    } catch (error) {
+      console.log("error", error);
+    }
   };
 
   return (
