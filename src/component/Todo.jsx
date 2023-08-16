4d2aa1b56235a51142f2ebb324b6d1f895b26ecd
yoru.lee <yoru.lee7@gmail.com>
2023-08-16 18:25:15 +0900

    feature : Assignment 5

--- src/component/Todo.jsx
+++ src/component/Todo.jsx
@@ -11,8 +11,20 @@ export const Todo = () => {
   }, []);
 
   return (
+    // Assignment 5
     <>
-      <div>todo</div>
+      <li>
+        <label>
+          <input type="checkbox" />
+          <span>TODO 1</span>
+        </label>
+      </li>
+      <li>
+        <label>
+          <input type="checkbox" />
+          <span>TODO 2</span>
+        </label>
+      </li>
     </>
   );
 };
